6c27e6cb0741af9fca193083befa052aadda1fae
PinkMonster0 <1053588980@qq.com>
2021-10-31 16:54:30 +0800

    Revert "Revert "Assets\Animations\Main Camera.controller""
    This reverts commit b45229f5d06a70258a4f6015d630c36962f37e3e.

--- Assets/Scripts/AnimationController.cs
+++ Assets/Scripts/AnimationController.cs
@@ -12,7 +12,7 @@ public class AnimationController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Camera_animator.enabled = true;
     }
 
     // Update is called once per frame
@@ -52,6 +52,7 @@ public class AnimationController : MonoBehaviour
         UI_animator.SetBool("IsPlay", false);
         Camera_animator.SetBool("IsWin", false);
         UI_animator.SetBool("IsWin", false);
+        Camera_animator.enabled = false;
     }
-
+    
 }
