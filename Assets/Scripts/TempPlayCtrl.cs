bb0b20ee6ddb520c15b8e09484a009cb4b4bfd31
PinkMonster0 <1053588980@qq.com>
2021-10-31 11:23:19 +0800

    add new fx and animation
    add new fx and animation

---
+++ Assets/Scripts/TempPlayCtrl.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempPlayCtrl : MonoBehaviour
+{
+    private Animator m_amimator;
+    private Animator m_fxAnimator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
